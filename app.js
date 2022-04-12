f15065cb2ad4d4c4cb52f0e6667070542b57bcf7
Esther Cho <helloecho12345@gmail.com>
2022-04-12 17:51:23 +0100

    installed pug templating engine to render name and email on localhost

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 // import express module and routes file into application
 const express = require('express');
+// require Node's native Path module which provides utilities for working with file and directory paths and helps build the path to our views folder
+const path = require('path');
 const routes = require('./routes/index');
 
 // creating a new Express app using the Express function and assigning it to an app variable
 const app = express();
+
+// using Path module -  __dirname returns the directory in which the currently executing script resides
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // tells app that whenever it receives a request from forward slash anything, it should use the routes file
 app.use('/', routes);
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,9 @@ const router = express.Router();
 
 // use router to respond to any requests to the root URL http://localhost:3000 with an "It works!" message
 router.get('/', (req, res) => {
-  res.send('It works!');
+  // tell route to use our new template using render method on Express's response object to send the rendered view to the client
+  res.render('form');
+  // res.send('It works!');
 });
 
 module.exports = router;
