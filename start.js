0eb717dea0b0d0a30094b935e6939d93bf392648
Esther Cho <helloecho12345@gmail.com>
2022-04-12 17:02:58 +0100

    added require function and port listening to start.js

---
+++ start.js
@@ -0,0 +1,7 @@
+// import Express app created in app.js
+const app = require('./app');
+
+// tell app to listen on port 3000 for incoming connections and output a message to the terminal to indicate the server is running
+const server = app.listen(3000, () => {
+  console.log('Express is running on port ${server.address().port}');
+});
