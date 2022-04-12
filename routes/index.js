ad316852a5d2ef3b90e8afd52b2248de8dd12c3d
Esther Cho <helloecho12345@gmail.com>
2022-04-12 21:45:28 +0100

    added POST route o index.js

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,13 @@ const router = express.Router();
 // use router to respond to any requests to the root URL http://localhost:3000 with an "It works!" message
 router.get('/', (req, res) => {
   // tell route to use our new template using render method on Express's response object to send the rendered view to the client
-  res.render('form');
+  // pass in an appropriate title for the template to display
+  res.render('form', { title: 'Registration Form'});
   // res.send('It works!');
 });
+
+router.post('/', (req, res) => {
+  res.render('form', { title: 'Registration Form'});
+})
 
 module.exports = router;
